fa650dda6a149d30c277689e28db116aa586b294
lcetin66 <lcetin@3ccs.com>
2026-04-07 17:09:17 +0200

    Update core files: Added SECRET_KEY, confirmed ISBN as String, and updated templates

--- data_models.py
+++ data_models.py
@@ -3,23 +3,30 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Author(db.Model):
+    __tablename__ = 'authors'
+    
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     birth_date = db.Column(db.Date, nullable=False)
     death_date = db.Column(db.Date, nullable=True)
     
+    books = db.relationship("Book", backref="author", lazy=True)
+    
     def __repr__(self):
-        return f"Author(id={self.id}, name='{self.name}', birth_date='{self.birth_date}', death_date='{self.death_date}')"
+        return f"<Author {self.id} {self.name}>"
 
     def __str__(self):
         return self.name
 
 
 class Book(db.Model):
+    __tablename__ = 'books'
+    
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     isbn = db.Column(db.String(13), nullable=False)
     title = db.Column(db.String(100), nullable=False)
     publication_year = db.Column(db.String(100), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'), nullable=False)
     
     def __repr__(self):
         return f"Book(id={self.id}, isbn='{self.isbn}', title='{self.title}', publication_year='{self.publication_year}')"
